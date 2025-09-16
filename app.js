0298dc9a94602983b9cbb3954061d71cf00061ae
Utaxy <omereren.gulden@gmail.com>
2025-09-16 06:02:56 +0300

    prod

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization','X-Requested-With']
+  allowedHeaders: ['Content-Type','Authorization','X-Requested-With','x-Requested-With']
 }));
 
 // Preflight'ı kesin cevapla
